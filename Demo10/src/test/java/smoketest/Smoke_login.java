35015fbc64dea0f77bf46d7929288e3bd5a385bf
user <user@user-PC>
2017-07-10 00:09:23 +0530

    my first commit

---
+++ Demo10/src/test/java/smoketest/Smoke_login.java
@@ -0,0 +1,34 @@
+package smoketest;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Smoke_login {
+	WebDriver driver;
+	
+	@Test
+	
+	public void login() throws Exception{
+		try{
+			driver=new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("http://toolsqa.com/");
+			driver.findElement(By.xpath(".//*[@id='primary-menu']/li[1]/a/span[1]/span/span")).click();
+			driver.close();			
+		}
+		catch(Exception e)
+		{
+			throw(e);
+		}
+		finally{
+			driver.close();
+		}
+		
+	}
+	
+
+}
